a04d2abe7abded6109d5f5d3afb97cf35dfc0444
CharlyKrDev <charlz17@hotmail.com>
2026-09-14 00:16:59 +0200

    feat: update href certificates

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -7,7 +7,7 @@ export const certifications: Certification[] = [
     issuer: "Coderhouse",
     year: 2024,
     achievement: "Top 10",
-    credentialUrl: "/public/certificates/66fb63adeec0c6579738f6bf.png",
+    credentialUrl: "/certificates/66fb63adeec0c6579738f6bf.png",
     skills: ["Node.js", "Express.js", "MongoDB", "REST APIs", "Supertest"],
   },
   {
@@ -16,7 +16,7 @@ export const certifications: Certification[] = [
     issuer: "Coderhouse",
     year: 2024,
     achievement: "Top 10",
-    credentialUrl:"/public/certificates/660da510b9ab6ed60fd2762a.png",
+    credentialUrl:"/certificates/660da510b9ab6ed60fd2762a.png",
     skills: ["JavaScript", "React", "Node.js", "Express.js", "MongoDB"],
   },
   {
@@ -24,7 +24,7 @@ export const certifications: Certification[] = [
     title: "GitHub Universe 2023 Cloud Skills Challenge",
     issuer: "Microsoft Learn",
     year: 2023,
-    credentialUrl:"/public/certificates/logro microsoft git.png",
+    credentialUrl:"/certificates/logro microsoft git.png",
     skills: ["Git", "GitHub", "GitHub Codespaces"],
   },
   {
@@ -33,7 +33,7 @@ export const certifications: Certification[] = [
     issuer: "Coderhouse",
     year: 2023,
     achievement: "Top 10",
-    credentialUrl:"/public/certificates/2-KjeVkFnY.png",
+    credentialUrl:"/certificates/2-KjeVkFnY.png",
     skills: ["JavaScript", "Asynchronous Programming", "API Integration"],
   },
   {
@@ -41,7 +41,7 @@ export const certifications: Certification[] = [
     title: "Data & Analytics — Starter Level",
     issuer: "Grupo Financiero Galicia / Datalytics",
     year: 2023,
-    credentialUrl:"/public/certificates/ACFrOgDyIAETaAf3nhRCGp21cYx5_zNInAIL0xxTYfvDJbIuO9puEzhx9dtpDGVswqVxVvBOYzb69RFKr5rAz2tA4dZUZNk8N5AjVTIPPLVwhYImCbhrjLhP8C4tRk3SQvE-0pXkh7IuAV-QYw17AnWZFFtrFd9c0YeNPqyNmA==.pdf",
+    credentialUrl:"/certificates/ACFrOgDyIAETaAf3nhRCGp21cYx5_zNInAIL0xxTYfvDJbIuO9puEzhx9dtpDGVswqVxVvBOYzb69RFKr5rAz2tA4dZUZNk8N5AjVTIPPLVwhYImCbhrjLhP8C4tRk3SQvE-0pXkh7IuAV-QYw17AnWZFFtrFd9c0YeNPqyNmA==.pdf",
     skills: ["SQL", "PostgreSQL", "Data Analysis"]
   },
   {
@@ -49,7 +49,7 @@ export const certifications: Certification[] = [
     title: "Web Programming",
     issuer: "UTN Buenos Aires",
     year: 2019,
-    credentialUrl:"/public/certificates/UTN-web-devoloper.png",
+    credentialUrl:"/certificates/UTN-web-devoloper.png",
     skills: ["HTML", "CSS", "JavaScript"],
   },
 ];
