24b980c82b0e0fba4a1c028d980d23351d04c248
CharlyKrDev <charlz17@hotmail.com>
2026-09-17 09:30:30 +0200

    feat(i18n): provide language context to app

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import "@fontsource-variable/inter";
 import "@fontsource-variable/jetbrains-mono";
 import "./index.css";
 import App from './App.tsx'
+import { LanguageProvider } from "./i18n/LanguageProvider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
-)
+);
